07fb64c119c4918b8a6792a058bf0342863414b5
adamtwig <terwilla@mail.gvsu.edu>
2017-01-18 12:35:57 -0500

    lab2 working

--- tcpserver.py
+++ tcpserver.py
@@ -11,14 +11,14 @@ import socket
 
 host = ''
 port = int(raw_input('port: '))
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(1)
-conn, addr = s.accept()
-print 'Connected by', addr
+
 while 1:
+	conn, addr = s.accept()
+	print 'Connected by', addr
 	data = conn.recv(1024)
-	#if not data: break
+	print "Received from client:", repr(data)
 	conn.sendall(data)
 conn.close()
