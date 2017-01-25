d1bc1ddf06342795ae1c10b2939ec2efcdfc148b
adamtwig <terwilla@mail.gvsu.edu>
2017-01-24 22:14:01 -0500

    small changes and documentation

--- project1/tcpserver.py
+++ project1/tcpserver.py
@@ -7,30 +7,38 @@ Purpose: CIS 457 Project 1
 Details: TCP File Transfer
 '''
 
-import socket
-import os
+import socket, os, sys
 
 host = ''
 port = int(raw_input('port: '))
+if port < 0 or port > 65536:
+	print "Invalid Port Number"
+	sys.exit(0)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(1)
 
+# multiple clients in sequence
 while 1:
 	print "Waiting for file request from client..."
 	conn, addr = s.accept()
 	print 'Connected by', addr
-	data = conn.recv(1024)
-	print "File request received from client for:", repr(data)
-	#filename = repr(data)
-	filename = data
+	
+	# receive filename from client
+	filename = conn.recv(1024)
+	print "File request received from client for:", repr(filename)
+	
+	# read this file 
 	myfile = open(filename, 'r')
 	myfilestr = myfile.read()
 	print "File sent to client."
-	#print os.path.getsize(data)
+	
+	# create a string of file length with 100-n blanks appended
+	# so server knows exact length of filelength string
 	myfilelen =  str(len(myfilestr))
 	myblanks = ' ' * (100-len(myfilelen))
 	mynewfilelen = myfilelen + myblanks
-	conn.send(mynewfilelen)
+	conn.sendall(mynewfilelen)
 	conn.sendall(myfilestr)
 conn.close()
