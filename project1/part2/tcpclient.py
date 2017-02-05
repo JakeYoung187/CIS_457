2d4775fc4a7bc4b2230f21867f4bf7aace525f18
adamtwig <terwilla@mail.gvsu.edu>
2017-02-05 17:43:53 -0500

    one client, multiple files

--- project1/part2/tcpclient.py
+++ project1/part2/tcpclient.py
@@ -7,16 +7,19 @@ Purpose: CIS 457 Project 1 Part 2
 Details: TCP Multi-threaded File Transfer
 '''
 
-import socket, os, sys, threading
+import socket, os, sys, threading, time
 
 class myThread (threading.Thread):
-	def __init__(self, s):
+	def __init__(self, s, filename):
 		threading.Thread.__init__(self)
 		self.s = s
+		self.filename = filename
 	
 	def run(self):
 		# client thread running
-		myfilename = raw_input('Enter filename to be received from server: ')
+		#myfilename = raw_input('Enter filename to be received from server: ')
+		myfilename = self.filename
+
 		if myfilename == "Server quitting":
 			print "\n",myfilename
 			self.s.close()
@@ -25,7 +28,7 @@ class myThread (threading.Thread):
 
 			# send filename to server
 			self.s.sendall(myfilename)
-			print "Sent filename to server."
+			print "\nSent filename to server."
 			#'''
 			# receive filelength as string from server
 			myfilelen = self.s.recv(100)
@@ -33,12 +36,12 @@ class myThread (threading.Thread):
 
 			# receive file as one string from server
 			myfilestr = self.s.recv(myfilelen)
-			print "Received file '" +  myfilename + "' from server"
+			print "\nReceived file '" +  myfilename + "' from server"
 
 			# write file to new_filename
 			mynewfile = open("new_" + myfilename, 'w')
 			mynewfile.write(myfilestr)
-			print("File written as: 'new_{}".format(myfilename))
+			print("\nFile written as: 'new_{}".format(myfilename))
 			#'''
 def main():
 
@@ -55,10 +58,17 @@ def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((host, port))
 
-	#while 1:
-	thread = myThread(s)
-	thread.start()
-	#print "starting thread"
+	while 1:
+		time.sleep(1)
+		myfilename = raw_input('\nEnter filename to be received from server: ')
+		if myfilename.lower() == "quit":
+			s.sendall("Client quitting")
+			s.close()
+			os._exit(1)
+		else:
+			thread = myThread(s, myfilename)
+			thread.start()
+			print "starting thread"
 
 if __name__ == "__main__":
     main()
--- project1/part2/tcpserver.py
+++ project1/part2/tcpserver.py
@@ -14,7 +14,7 @@ class myThread (threading.Thread):
 		threading.Thread.__init__(self)
 		self.conn = conn
 	def run(self):
-		#while 1:
+		while 1:
 			filename = self.conn.recv(1024)
 			if filename == "Client quitting":
 				print "\n",filename
@@ -49,13 +49,13 @@ def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port))
 	
-	#while 1:
-	s.listen(1)
-	conn, addr = s.accept()
-	print 'Connected by', addr
-	thread = myThread(conn)
-	thread.start()
-	#print "starting thread"	
+	while 1:
+		s.listen(1)
+		conn, addr = s.accept()
+		print 'Connected by', addr
+		thread = myThread(conn)
+		thread.start()
+		print "starting thread"	
 	
 if __name__ == "__main__":
     main()
