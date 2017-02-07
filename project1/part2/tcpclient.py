981ef5e7cb59c719f50cc5fa05e189aec4158cc0
Ellysa <stantoel@mail.gvsu.edu>
2017-02-06 19:21:12 -0500

    yaas lady gaga

--- project1/part2/tcpclient.py
+++ project1/part2/tcpclient.py
@@ -7,7 +7,7 @@ Purpose: CIS 457 Project 1 Part 2
 Details: TCP Multi-threaded File Transfer
 '''
 
-import socket, os, sys, threading, time
+import socket, os, sys, threading, time, re
 
 class myThread (threading.Thread):
 	def __init__(self, s, filename):
@@ -16,59 +16,68 @@ class myThread (threading.Thread):
 		self.filename = filename
 	
 	def run(self):
-		# client thread running
-		#myfilename = raw_input('Enter filename to be received from server: ')
-		myfilename = self.filename
+		self.s.sendall(self.filename)
+		if self.filename == "ls":
+			print "\nSent request for list of files in server."
+			filelist = self.s.recv(1024)
+			print filelist
+		else:	
+			print "\nSent filename to server."
+			
+			myfilelen = self.s.recv(20).strip()
+			if myfilelen == "File N/A":
+				print "\nInvalid file. Type \"ls\" to see available files on server."
+			else:
+				# receive filelength as string from server
+				myfilelen = int(myfilelen)
 
-		if myfilename == "Server quitting":
-			print myfilename
-			self.s.close()
-			os._exit(1)
-		else:
+				# receive file as one string from server
+				myfilestr = self.s.recv(myfilelen)
+				print "\nReceived file '" +  self.filename + "' from server"
 
-			# send filename to server
-			self.s.sendall(myfilename)
-			print "Sent filename to server."
-			#'''
-			# receive filelength as string from server
-			myfilelen = self.s.recv(100)
-			myfilelen = int(myfilelen.strip())
+				# write file to new_filename
+				mynewfile = open("new_" + self.filename, 'w')
+				mynewfile.write(myfilestr)
+				print("\nFile written as: 'new_{}".format(self.filename))
 
-			# receive file as one string from server
-			myfilestr = self.s.recv(myfilelen)
-			print "Received file '" +  myfilename + "' from server"
+#Regular Expression check for valid host name
+#http://stackoverflow.com/questions/2532053/validate-a-hostname-string
+def is_valid_hostname(hostname):
+	if len(hostname) > 255:
+		return False
+	if hostname[-1] == ".":
+		hostname = hostname[:-1] # strip exactly one dot from the right, if present
+	allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+	return all(allowed.match(x) for x in hostname.split("."))
 
-			# write file to new_filename
-			mynewfile = open("new_" + myfilename, 'w')
-			mynewfile.write(myfilestr)
-			print("File written as: 'new_{}".format(myfilename))
-			#'''
+
 def main():
-
 	host = raw_input('host: ')
-	if host != "127.0.0.1":
-		print "Non loopback host"
-		sys.exit(0)
 
-	port = int(raw_input('port: '))
-	if port < 0 or port > 65536:
-		print "Invalid Port Number"
-		sys.exit(0)
+	while not is_valid_hostname(host):
+		print "Invalid host, try again."
+		host = raw_input('host: ')
+
+	port = raw_input('port: ')
+	while not port.isdigit() or int(port) < 0 or int(port) > 65536:
+		print "Invalid Port Number, try again."
+		port = raw_input('port: ')
+
+	port = int(port)
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((host, port))
 
 	while 1:
-		time.sleep(1)
-		myfilename = raw_input('Enter filename to be received from server: ')
+		time.sleep(0.1)
+		myfilename = raw_input('\nEnter filename to be received from server: ')
 		if myfilename.lower() == "quit":
-			s.sendall("Client quitting")
+			s.sendall("Client quitting.")
 			s.close()
 			os._exit(1)
 		else:
 			thread = myThread(s, myfilename)
 			thread.start()
-	#		print "starting thread"
 
 if __name__ == "__main__":
     main()
--- project1/part2/tcpserver.py
+++ project1/part2/tcpserver.py
@@ -15,48 +15,66 @@ class myThread (threading.Thread):
 	def __init__(self, conn):
 		threading.Thread.__init__(self)
 		self.conn = conn
+		self.serverfl = os.listdir(".")
 	def run(self):
 		global clientcount
 		while 1:
 			filename = self.conn.recv(1024)
-			if filename == "Client quitting":
-				print filename
-		#		self.conn.close()
-		#		os._exit(1)	
+			if filename == "Client quitting.":
+				print("\nClient {} quitting".format(clientcount))
 				clientcount -= 1
 				if (clientcount == 0):
+					print "\nServer quitting."
 					self.conn.close()
 					os._exit(1)	
 
-			else:
+			elif filename == "ls":
+				print("\nClient {} requested a list of files on server.".format(clientcount))
+				self.conn.sendall(str(self.serverfl))
+			
+			elif filename not in self.serverfl:
+				print("\nClient {} requested a file not on server.".format(clientcount))
+				myInvalidMsg = appendBlanks("File N/A",20)
+				self.conn.sendall(myInvalidMsg)
+			
+			elif filename != '':
 				# receive filename from client
-				print "File request received from client for:", filename
+				print "\nFile request received from client for:", filename
 
 				# read this file 
 				myfile = open(filename, 'r')
 				myfilestr = myfile.read()
-		#		print myfilestr
-				print "File sent to client."
+				#print "\nFile sent to client."
 
-				# create a string of file length with 100-n blanks appended
+				# create a string of file length with 20-n blanks appended
 				# so server knows exact length of filelength string
 				myfilelen =  str(len(myfilestr))
-				myblanks = ' ' * (100-len(myfilelen))
-				mynewfilelen = myfilelen + myblanks
+				#myblanks = ' ' * (20-len(myfilelen))
+				#mynewfilelen = myfilelen + myblanks
+				mynewfilelen = appendBlanks(myfilelen,20)
 				self.conn.sendall(mynewfilelen)
 				self.conn.sendall(myfilestr)
+
+def appendBlanks(myStr, myLen):
+	myBlanks = ' ' * (myLen-len(myStr))
+	return myStr + myBlanks
 
 def main():
 
 	host = ''
-	port = int(raw_input('port: '))
-	if port < 0 or port > 65536:
-		print "Invalid Port Number"
-		sys.exit(0)
+	port = -1
+	
+	port = raw_input('port: ')
+	while not port.isdigit() or int(port) < 0 or int(port) > 65536:
+		print "Invalid Port Number, try again."
+		port = raw_input('port: ')
 
+	port = int(port)
+
+	print "Waiting for client(s) to connect."
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port))
-		
+
 	global clientcount
 	
 	while 1:
@@ -66,11 +84,6 @@ def main():
 		clientcount += 1	
 		thread = myThread(conn)
 		thread.start()
-		#print "starting thread"	
-		
-		#if (clientcount == 0):
-		#	conn.close()
-		#	os._exit(1)	
 
 if __name__ == "__main__":
     main()
