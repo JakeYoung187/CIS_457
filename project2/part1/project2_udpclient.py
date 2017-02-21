19bd087d3de8483658dbaaf586533cce95b4f35c
adamtwig <terwilla@mail.gvsu.edu>
2017-02-21 14:34:56 -0500

    making progress

--- project2/part1/project2_udpclient.py
+++ project2/part1/project2_udpclient.py
@@ -9,8 +9,18 @@ Details: UDP File Transfer
 Client program
 '''
 import socket, os, sys, re
+class filePacket(object):
+	def __init__(self, size=None, index=None, data=None, last=None):
+		self.size = size
+		self.index = index
+		self.data = data
+		self.last = last
+		self.ackSent = 0
+	def __str__(self):
+		return "Size: {}\n Index: {}\n Last: {}\n Data: {}".format(self.size, self.index, self.last, self.data)
 
-quitStrs = ['quit', 'exit']
+	def __repr__(self):
+		return str(self)
 
 #Regular Expression check for valid host name
 #http://stackoverflow.com/questions/2532053/validate-a-hostname-string
@@ -23,36 +33,69 @@ def is_valid_hostname(hostname):
 	allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
 	return all(allowed.match(x) for x in hostname.split("."))
 
-def main():
+def main(argv):
 	
-	host = raw_input('host: ')
-
-	# use regular expression function
-	while not is_valid_hostname(host):
-		print "Invalid host, try again."
+	# either enter host/port by command line or user input
+	if len(argv) == 3:
+		host = argv[1]
+		port = int(argv[2])
+	
+	else:
+		
 		host = raw_input('host: ')
 
-	port = raw_input('port: ')
+		# use regular expression function
+		while not is_valid_hostname(host):
+			print "Invalid host, try again."
+			host = raw_input('host: ')
 
-	# check for non-characters and negative or large port numbers
-	while not port.isdigit() or int(port) < 0 or int(port) > 65536:
-		print "Invalid Port Number, try again."
 		port = raw_input('port: ')
 
-	# cast port only when know it's valid
-	port = int(port)
+		# check for non-characters and negative or large port numbers
+		while not port.isdigit() or int(port) < 0 or int(port) > 65536:
+			print "Invalid Port Number, try again."
+		port = raw_input('port: ')
+
+		# cast port only when know it's valid
+		port = int(port)
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	while 1:
-		message = raw_input('\nEnter your message: ')
-		if any(x in message.lower() for x in quitStrs):
-			print "\nClient quitting..."
-			sys.exit(-1)
-		s.sendto(message, (host, port))
-		data, server_addr = s.recvfrom(1024)
-		print "Received echo: '{}' from {}".format(data, server_addr)
+	# only one file for now
+	#while 1:
+	myfilename = raw_input('\nEnter filename to be received from server: ')
+	if myfilename.lower() == "quit":
+		s.close()	
+		sys.exit(-1)
+	else:
+		s.sendto(myfilename, (host, port))
+		packetList = []
+		while 1:
+			packetStr, server_addr = s.recvfrom(1024)
+			print packetStr, "\n\n"
+			x = filePacket()
+			for line in packetStr.split("\n"):
+				classVar = line.split(":")[0]
+				classVal = line.split(":")[1]
+				# could probably clean up with switch/cases
+				if classVar == "Size":
+					x.size = classVal
+				elif classVar == "Index":
+					x.index = classVal
+				elif classVar == "Last":
+					x.last = classVal
+				elif classVar == "Data":
+					x.data = classVal
+
+			packetList.append(x)
+		
+			if x.last:
+				break
+
+		for packet in packetList:
+			print packet
+
 	s.close()
 
 if __name__ == "__main__":
-    main()
+	main(sys.argv)
 
--- project2/part1/project2_udpserver.py
+++ project2/part1/project2_udpserver.py
@@ -8,30 +8,69 @@ Details: UDP File Transfer
 
 Server program
 '''
-import socket, os, sys, re
+import socket, os, sys
 
-def main():
-	
+# http://stackoverflow.com/questions/15599639/whats-perfect-counterpart-in-python-for-while-not-eof
+from functools import partial
+
+class filePacket(object):
+	def __init__(self, size, index, data, last):
+		self.size = size
+		self.index = index
+		self.data = data
+		self.last = last 
+		self.ackRecv = 0
+
+	def __str__(self):
+		return "Size: {}\n Index: {}\n Last: {}\n Data: {}".format(self.size, self.index, self.last, self.data)
+
+	def __repr__(self):
+		return str(self)
+
+def main(argv):
+
 	host = ''
-	port = raw_input('port: ')
 
-	# check for non-characters and negative or large port numbers
-	while not port.isdigit() or int(port) < 0 or int(port) > 65536:
-		print "Invalid Port Number, try again."
+	if len(argv) == 2:
+		port = int(argv[1])
+
+	else:
 		port = raw_input('port: ')
 
-	# cast port only when know it's valid
-	port = int(port)
-	
+		# check for non-characters and negative or large port numbers
+		while not port.isdigit() or int(port) < 0 or int(port) > 65536:
+			print "Invalid Port Number, try again."
+			port = raw_input('port: ')
+
+		# cast port only when know it's valid
+		port = int(port)
+
+	print "Waiting for client(s) to request file(s)..."	
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.bind((host, port))
 
-	while 1:
-		data, client_addr = s.recvfrom(1024)
-		print "\nReceived message: {}".format(data)
-		s.sendto(data, client_addr)
-		print "Echoed back to {}".format(client_addr)
+	#while 1:
+	filename, client_addr = s.recvfrom(1024)
+	if filename in os.listdir("."):
+		
+		# receive filename from client
+		print ("\nFile request received from client for: {}".format(filename))
+		
+		# read this file
+		packetDataSize = 32
+		packetIndex = 0
+		packetList = []
+		with open(filename, 'r') as myfile:
+			for fileChunk in iter(partial(myfile.read, packetDataSize), ''):
+				x = filePacket(packetDataSize, packetIndex, fileChunk, 0)
+				packetList.append(x)
+				packetIndex+=1
+
+		for packet in packetList:
+			print "Sending packet {}".format(packet.index)
+			s.sendto(str(packet), client_addr)
+
 	s.close()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
