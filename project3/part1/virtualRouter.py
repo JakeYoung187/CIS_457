25abb87e302a8b80a6d7ff378f32225eedaaaf1a
Ellysa <stantoel@mail.gvsu.edu>
2017-03-24 13:55:00 -0400

    Exploring sockets and arp.

--- project3/part1/virtualRouter.py
+++ project3/part1/virtualRouter.py
@@ -14,19 +14,25 @@ import netifaces
 from struct import *
 
 def main(argv):    
-    print netifaces.interfaces()
-
-    print netifaces.ifaddresses('lo')
-    
-    print netifaces.ifaddresses('r1-eth0')
+    #print netifaces.interfaces()
+    #print netifaces.ifaddresses('lo')
+    #print netifaces.ifaddresses('r1-eth0')
 
     try: 
-        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        #s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x003))
         print "Socket created correctly."
         print s
     except socket.error , msg:
         print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
         sys.exit(-1)
- 
+    
+    while 1:
+        print "In While Loop!"
+        raw_packet, host_addr = s.recvfrom(2048)
+        print raw_packet
+        
+        
+
 if __name__ == "__main__":
     main(sys.argv)
