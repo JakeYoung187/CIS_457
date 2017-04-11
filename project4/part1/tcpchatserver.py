df0e5218958a359b843d6d542effdffc4c9f87ad
Ellysa <stantoel@mail.gvsu.edu>
2017-04-11 14:28:11 -0400

    File/folders created.

---
+++ project4/part1/tcpchatserver.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+'''
+Developer:  Adam Terwilliger, Ellysa Stanton
+Version:    April 12, 2017
+Assignment: CIS 457 Project 4 Part 1
+            TCP Encrypted Chat Program
+Objectives: Implementing a multi-client/server chat system.
+            Learning how to encrypt/decrypt in code.
+'''
+
+import sys
+
+def main(argv):
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv)
